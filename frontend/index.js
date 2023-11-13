e798a2d7fce1b3974612dac815750f94d7930fda
socheatathai <socheatathai62@gmail.com>
2023-11-13 19:50:20 +0700

    add google map

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,9 @@
-
+// script.js
+function initMap() {
+  // Create a map centered at a specific location
+  var map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
+console.log(initMap);

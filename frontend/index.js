4b1fb117c126d41f7878fe3037a9086fa62e7f78
socheatathai <socheatathai62@gmail.com>
2023-11-19 00:06:41 +0700

    add home page

--- frontend/index.js
+++ frontend/index.js
@@ -8,12 +8,59 @@ function initMap() {
 }
 console.log(initMap);
 
-
 // popular products
-var likeBtns = document.querySelectorAll('.like-btn');
-likeBtns.forEach(btn => {
-    btn.addEventListener('click', function() {
-        this.classList.toggle('fas');
-        this.classList.toggle('far');
+var likeBtns = document.querySelectorAll(".like-btn");
+likeBtns.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    this.classList.toggle("fas");
+    this.classList.toggle("far");
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const slider = document.querySelector(".slider");
+  const slides = document.querySelectorAll(".slide");
+  const prevBtn = document.getElementById("prevBtn");
+  const nextBtn = document.getElementById("nextBtn");
+  const dotsContainer = document.querySelector(".slider-dots");
+
+  let index = 0;
+
+  // Create dots
+  slides.forEach((_, i) => {
+    const dot = document.createElement("span");
+    dot.classList.add("dot");
+    dot.addEventListener("click", () => goToSlide(i));
+    dotsContainer.appendChild(dot);
+  });
+
+  const dots = document.querySelectorAll(".dot");
+  dots[0].classList.add("active");
+
+  // Event listeners for navigation buttons
+  prevBtn.addEventListener("click", () => goToSlide(index - 1));
+  nextBtn.addEventListener("click", () => goToSlide(index + 1));
+
+  // Autoplay functionality
+  setInterval(() => {
+    goToSlide(index + 1);
+  }, 5000); // Adjust the interval (in milliseconds) as needed
+
+  function goToSlide(i) {
+    if (i < 0) {
+      index = slides.length - 1;
+    } else if (i >= slides.length) {
+      index = 0;
+    } else {
+      index = i;
+    }
+
+    // Update the transform property to show the correct slide
+    slider.style.transform = `translateX(${-index * 100}%)`;
+
+    // Update the active dot
+    dots.forEach((dot, i) => {
+      dot.classList.toggle("active", index === i);
     });
+  }
 });

154b42a2e868392570df5d4e8609f2d5c23215ca
socheatathai <socheatathai62@gmail.com>
2023-12-01 00:20:19 +0700

    product detail

--- frontend/pages/detail-products/detail-product.js
+++ frontend/pages/detail-products/detail-product.js
@@ -1,47 +1,24 @@
-let currentSlide = 0;
-
-function showSlide(index) {
-  let slides = document.getElementsByClassName("carousel-item");
-  slides[currentSlide].classList.remove("active");
-  currentSlide = index;
-  slides[currentSlide].classList.add("active");
+function increment() {
+  var quantityInput = document.getElementById("quantity");
+  var currentQuantity = parseInt(quantityInput.value);
+  quantityInput.value = currentQuantity + 1;
 }
 
-function nextSlide() {
-  let totalSlides = document.getElementsByClassName("carousel-item").length;
-  showSlide((currentSlide + 1) % totalSlides);
+function decrement() {
+  var quantityInput = document.getElementById("quantity");
+  var currentQuantity = parseInt(quantityInput.value);
+  if (currentQuantity > 1) {
+    quantityInput.value = currentQuantity - 1;
+  }
 }
 
-window.onload = function () {
-  showSlide(0);
-  setInterval(nextSlide, 4000);
-};
- function handleSizeClick(element, size) {
-   // Remove the 'selected' class from all elements
-   document.querySelectorAll(".size-img").forEach(function (el) {
-     el.classList.remove("selected");
-   });
-
-   // Add the 'selected' class to the clicked element
-   element.classList.add("selected");
-
-   // Placeholder for your custom logic
-   console.log("Selected size: " + size);
-   // Add your logic for handling the size click event
-   // For example, you can update a variable or perform other actions
- }
-
-function handleSizeClick(element, size) {
-  // Remove the 'selected' class from all elements
-  document.querySelectorAll(".size-img").forEach(function (el) {
+function changeColor(element) {
+  // Remove 'selected' class from all elements
+  var elements = document.querySelectorAll(".img-size-small");
+  elements.forEach(function (el) {
     el.classList.remove("selected");
   });
 
-  // Add the 'selected' class to the clicked element
+  // Add 'selected' class to the clicked element
   element.classList.add("selected");
-
-  // Placeholder for your custom logic
-  console.log("Selected size: " + size);
-  // Add your logic for handling the size click event
-  // For example, you can update a variable or perform other actions
 }
